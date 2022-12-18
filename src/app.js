5bbefae658839cf8dc0841a4f4e386b265bdd618
Logan Brown <logan.brown@tylertech.com>
2022-12-18 17:12:44 -0500

    Updated serialport connection

--- src/app.js
+++ src/app.js
@@ -1,95 +1,107 @@
+import { EventEmitter } from 'node:events';
+const eventEmitter = new EventEmitter();
 import * as Service from './Service.js';
+import { SerialPort } from 'serialport'
 const arg = process.argv.slice(2)[0];
 
-const getMac = async () => {
-    if (!arg) return;
-    if (arg.toString() === '-mac') {
-        var mac = process.argv.slice(2)[1];
-        mac = mac.replace(/-/g, ':').toUpperCase();
-        const addMacReg = "^[0-9A-F]{2}:[0-9A-F]{2}:[0-9A-F]{2}:[0-9A-F]{2}:[0-9A-F]{2}:[0-9A-F]{2}$";
-        const regex = new RegExp(addMacReg);
-        if (regex.test(mac)) {
-            return mac;
-        } else {
-            console.log('MAC address is not valid');
-            return undefined;
+// const getMac = () => {
+//     if (!arg) return;
+//     if (arg.toString() === '-mac') {
+//         var mac = process.argv.slice(2)[1];
+//         mac = mac.replace(/-/g, ':').toUpperCase();
+//         const addMacReg = "^[0-9A-F]{2}:[0-9A-F]{2}:[0-9A-F]{2}:[0-9A-F]{2}:[0-9A-F]{2}:[0-9A-F]{2}$";
+//         const regex = new RegExp(addMacReg);
+//         if (regex.test(mac)) {
+//             return mac;
+//         } else {
+//             console.log('MAC address is not valid');
+//             return undefined;
+//         }
+//     }
+// };
+
+// Serial port options
+const options = {
+    baudRate: 115200,
+    path: '/dev/ttyUSB0',
+};
+
+// Create serial port instance with options
+const serialport = new SerialPort(options)
+
+// Connect to serial port
+serialport.on('open', () => {
+    // Emit connected event
+    eventEmitter.emit('connected');
+
+    // Write to serial port
+    serialport.write('ATZ', (error) => {
+        if (error) {
+            console.log(`${error}`);
         }
-    }
-};
-
-var connected = false;
-
-const pairMode = async () => {
-    const macAddress = await getMac();
-    Service.execute("sudo bluetoothctl").then((result) => {
-        Service.execute("power on").then((result) => {
-            Service.execute("pairable on").then((result) => {
-                Service.execute("agent on").then((result) => {
-                    Service.execute("default-agent").then((result) => {
-                        Service.execute("scan on").then((result) => {
-                            // Pair with device
-                            Service.execute(`pair ${macAddress}`).then((result) => {
-                                // Trust device
-                                Service.execute(`trust ${macAddress}`).then((result) => {
-                                    // Bind device to serial port
-                                    Service.execute(`sudo rfcomm bind rfcomm0 ${macAddress}`).then((result) => {
-                                        connected = true;
-                                    }).catch((error) => {
-                                        console.log(`Failed to bind device to serial port: ${error}`);
-                                    });
-                                }).catch((error) => {
-                                    console.log(`Failed to trust device: ${error}`);
-                                });
-                            }).catch((error) => {
-                                console.log(`Failed to pair device: ${error}`);
-                            });
-                        }).catch((error) => {
-                            console.log(`Failed to scan: ${error}`);
-                        });
-                    }).catch((error) => {
-                        console.log(`Failed to set default agent: ${error}`);
-                    });
-                }).catch((error) => {
-                    console.log(`Failed to set agent: ${error}`);
-                });
-            }).catch((error) => {
-                console.log(`Failed to set pairable: ${error}`);
-            });
-        }).catch((error) => {
-            console.log(`Failed to set power: ${error}`);
-            console.log(`Attempting to start bluetoothd...`);
-            Service.execute("sudo systemctl start bluetooth").then((result) => {
-                console.log(`Bluetoothd started successfully`);
-                // Restart function
-                pairMode();
-            }).catch((error) => {
-                console.log(`Failed to start bluetoothd: ${error}`);
-            });
-        });
-    }).catch((error) => {
-        console.log(error);
     });
-};
 
-setInterval(() => {
-    if (connected) {
-        Service.execute("screen /dev/rfcomm0").then((result) => {
-            Service.execute("ath0").then((result) => {
-                // Automatic protocol detection
-                Service.execute("atsp0").then((result) => {
-                    Service.getVin().then((result) => {
-                        console.log(`VIN: ${result}`);
-                    }).catch((error) => {
-                        console.log(`Failed to get VIN: ${error}`);
-                    });
-                }).catch((error) => {
-                    console.log(`Failed to set protocol: ${error}`);
-                });
-            }).catch((error) => {
-                console.log(`Failed to set header: ${error}`);
-            });
-        }).catch((error) => {
-            console.log(`Failed to connect to device: ${error}`);
-        });
-    }
-}, 1000);
+    // Disable echo
+    serialport.write('ATE0', (error) => {
+        if (error) {
+            console.log(`${error}`);
+        }
+    });
+
+    // Disable linefeeds and carriage returns
+    serialport.write('ATL0', (error) => {
+        if (error) {
+            console.log(`${error}`);
+        }
+    });
+
+    // Disable spaces in responses
+    serialport.write('ATS0', (error) => {
+        if (error) {
+            console.log(`${error}`);
+        }
+    });
+
+    // Disable headers in responses
+    serialport.write('ATH0', (error) => {
+        if (error) {
+            console.log(`${error}`);
+        }
+    });
+
+    // Set protocol to automatic
+    serialport.write('ATSP0', (error) => {
+        if (error) {
+            console.log(`${error}`);
+        }
+    });
+});
+
+// Catch serial port permission errors
+serialport.on('permissionError', (error) => {
+    console.log(`${error}`);
+});
+
+// Catch serial port errors
+serialport.on('error', (error) => {
+    console.log(`${error}`);
+});
+
+// Catch serial port data
+serialport.on('data', (data) => {
+    console.log(`${data}`);
+});
+
+// Catch serial port disconnect
+serialport.on('disconnect', () => {
+    // Emit disconnected event
+    eventEmitter.emit('disconnected');
+});
+
+eventEmitter.on('connected', () => {
+    console.log('Device connected');
+});
+
+eventEmitter.on('disconnected', () => {
+    console.log('Device disconnected');
+});
