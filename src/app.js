5eaa004052616713a5db91ff1503f44b6aafd274
Logan Brown <logan.brown@tylertech.com>
2023-06-17 21:40:49 -0400

    Updates

--- src/app.js
+++ src/app.js
@@ -9,105 +9,103 @@ import { ReadlineParser } from '@serialport/parser-readline';
 import { autoDetect } from '@serialport/bindings-cpp';
 const Binding = autoDetect();
 import { SerialPort } from 'serialport';
-
-// Socket.io server
 import http from 'http';
 const server = http.createServer(app);
 import { Server } from 'socket.io';
 const io = new Server(server);
-
 app.use(express.json())
 app.use('/', express.static(path.join(__dirname, 'public')))
 
-server.listen(port, () => {
-    console.log(`Express listening on port ${port}`)
-});
-
-app.get('/restart', function (req, res, next) {
-    res.redirect('/');
-    process.exit(1);
-});
-
 const OpenSerialPort = () => {
     return new Promise((resolve, reject) => {
-        Binding.list().then((ports) => {
-            Object.keys(ports).forEach((key) => {
-                console.log(`Found serial device: ${ports[key].path}`)
-                resolve(ports[key].path);
+        const interval = setInterval(() => {
+            Binding.list().then((ports) => {
+                if (ports.length > 0) clearImmediate(interval);
+                Object.keys(ports).forEach((key) => {
+                    resolve(ports[key].path);
+                });
+            }).catch((err) => {
+                reject(err);
             });
-        });
+        }, 1000);
     });
 };
-
-const options = {
-    baudRate: 115200,
-    autoOpen: false,
-    dataBits: 8,
-    path: await OpenSerialPort(),
-    parity: 'none',
-    flowControl: false,
-};
-
-class Serial {
-    constructor (options) {
-        this.options = options;
-        this.serialport = new SerialPort(this.options);
-        this.parser = this.serialport.pipe(new ReadlineParser());
-    }
-    connect() {
-        this.serialport.open();
-
-        this.serialport.on('open', () => {
-            // Connection opened
-        });
-
-        this.serialport.on('close', () => {
-            // Disconnect
-            console.log('Connection terminated')
-        });
-
-        this.serialport.on('error', (err) => {
-            console.log(`Error: ${err.message}`);
-        });
-
-        this.parser.on('data', (data) => {
-            console.log(`Data received: ${data}`);
-            io.emit('data', data);
-        });
-    }
-    disconnect() {
-        this.serialport.close();
-    }
-}
-
-let serial = new Serial(options);
 
 io.on('connection', async (socket) => {
-    // Check if serial port is open every 5 seconds
-    setInterval(() => {
-        if (serial.serialport.isOpen) {       
-            socket.emit('serialport', { status: 'open', path: options.path });
-        } else {
-            socket.emit('serialport', { status: 'closed' });
-        }
-    }, 50);
+    const options = {
+        baudRate: 115200,
+        autoOpen: false,
+        dataBits: 8,
+        path: await OpenSerialPort(),
+        parity: 'none',
+        flowControl: false,
+    };
+    
+    const serialport = new SerialPort(options);
 
-    socket.on('_connect', () => {
-        serial.connect();
+    // Check if the serial port is already open before opening it
+    if (serialport.isOpen) {
+        console.log(`Serial port ${options.path} is already open`);
+        socket.emit('connected', { path: options.path });
+    } else {
+        // Open the serial port
+        serialport.open((err) => {
+            if (err) console.log(err.message);
+        });
+    }
 
-        socket.on('command', (data) => {
-            serial.serialport.write('010D\r\n', (err) => {
-                if (err) {
-                    console.log(`Error on write: ${err.message}`);
-                    io.emit('data', 'An error occurred while writing to the serial port');
-                } else {
-                    console.log(`Command received: ${data}`);
+    // Create a parser object
+    const parser = serialport.pipe(new ReadlineParser());
+
+    serialport.on('open', () => {
+        console.log(`Serial port opened at ${options.path}`);
+        socket.emit('connected', { path: options.path });
+    });
+    
+    // Handle closed connection events
+    serialport.on('close', () => {
+        console.log('Serial port closed');
+        socket.emit('disconnected');
+    });
+
+    let busy = 0;
+    // Handle data sent from the client to the serial port
+    socket.on('data', (data) => {
+        serialport.write(data, (err) => {
+            busy = 1;
+            console.log(`Sent data: ${data}`);
+            if (err) console.log(err.message);
+            // Send timeout error if the serial port did not respond within 5 seconds
+            setTimeout(() => {
+                if (busy == 1) {
+                    console.log('Serial port timed out');
+                    socket.emit('message', 'Serial port timed out');
                 }
-            });
+            }, 5000);
+        });
+    });
+        
+    // Handle incoming data from the serial port
+    parser.on('data', (data) => {
+        busy = 0;
+        console.log(`Received data: ${data}`);
+    });
+
+    // Handle unexpected disconnections
+    socket.on('disconnect', () => {
+        // Make sure the serial port is open before closing it
+        if (serialport.isOpen) serialport.close((err) => {
+            if (err) console.log(err.message);
         });
     });
 
-    socket.on('_disconnect', () => {
-        serial.disconnect();
+    // Handle errors
+    serialport.on('error', (err) => {
+        console.log(err.message);
     });
+    
+});
+
+server.listen(port, () => {
+    console.log(`Express listening on port ${port}`)
 });
--- src/public/js/core.js
+++ src/public/js/core.js
@@ -1,152 +1,69 @@
 const socket = io();
 const statusText = document.getElementById('status');
-const connect = document.getElementById('connect');
 const disconnect = document.getElementById('disconnect');
-const retry = document.getElementById('retry');
+const connect = document.getElementById('connect');
 const command = document.getElementById('command');
 const send = document.getElementById('send');
 const output = document.getElementById('output');
 
-socket.on('serialport', (data) => {
-    if (data.status == 'open') {
-        statusText.innerHTML = `Status: Connected to ${data.path}`;
-        statusText.style.color = '#238636';
-        disable('connect')
-        enable('disconnect')
-        disable('retry')
-        enable('send')
-        enable('command')
-    } else if (data.status == 'closed') {
-        statusText.innerHTML = 'Status: Disconnected';
-        statusText.style.color = '#d93025';
-        enable('connect')
-        disable('disconnect')
-        enable('retry')
-        disable('send')
-        disable('command')
-    } else if (data.status == 'error') {
-        statusText.innerHTML = `Error: ${data.message}`;
-        statusText.style.color = '#d93025';
-        enable('connect')
-        disable('disconnect')
-        enable('retry')
-        disable('send')
-        disable('command')
-    }
-});
-
-// Check if connection is stuck
-setTimeout(() => {
-    if (statusText.innerHTML == 'Connecting...' || statusText.innerHTML == 'Checking Connection...') {
-        statusText.innerHTML = 'Error: Unable to locate a serial port';
-        statusText.style.color = '#d93025';
-        disable('connect')
-        disable('disconnect')
-        enable('retry')
-        disable('send')
-        disable('command')
-    }
-}, 1000);
-
-socket.on('connect', () => {
-    statusText.innerHTML = 'Checking Connection...';
-    statusText.style.color = '#fc8c03';
+socket.on('connected', (data) => {
+    statusText.innerHTML = `Status: Connected to ${data.path}`;
+    statusText.style.color = '#238636';
+    enable('disconnect')
     disable('connect')
-    disable('disconnect')
-    disable('retry')
-    disable('send')
-    disable('command')
-    // Check if connection is stuck
-    setTimeout(() => {
-        if (statusText.innerHTML == 'Connecting...' || statusText.innerHTML == 'Checking Connection...') {
-            statusText.innerHTML = 'Error: Unable to connect to serial port';
-            statusText.style.color = '#d93025';
-            disable('connect')
-            disable('disconnect')
-            enable('retry')
-            disable('send')
-            disable('command')
-        }
-    }, 1000);
+    enable('send')
+    enable('command')
 });
 
-// Lost connection
-socket.on('disconnect', () => {
+socket.on('disconnected', () => {
     statusText.innerHTML = 'Status: Disconnected';
     statusText.style.color = '#d93025';
-    enable('connect')
+    socket.disconnect();
     disable('disconnect')
-    disable('retry')
+    enable('connect')
+    disable('send')
+    disable('command')
+});
+
+socket.on('message', (message) => {
+    let p = document.createElement('p');
+    p.innerHTML = `${message}<br>`;
+    output.appendChild(p);
+});
+
+socket.on("connect_error", () => {
+    statusText.innerHTML = 'Status: Connection error';
+    statusText.style.color = '#d93025';
+    disable('disconnect')
+    enable('connect')
     disable('send')
     disable('command')
 });
 
 if (connect) {
     connect.addEventListener('click', () => {
-        socket.emit('_connect');
+        socket.connect();
+        statusText.innerHTML = 'Status: Connecting...';
+        statusText.style.color = '#fc8c03';
         disable('connect')
-        disable('disconnect')
-        disable('retry')
         disable('send')
         disable('command')
-        statusText.style.color = '#fc8c03';
-        statusText.innerHTML = 'Connecting...';
-        // Check if connection is stuck
-        setTimeout(() => {
-            if (statusText.innerHTML == 'Connecting...') {
-                statusText.innerHTML = 'Error: Unable to connect to serial port';
-                statusText.style.color = '#d93025';
-                disable('connect')
-                disable('disconnect')
-                enable('retry')
-                disable('send')
-                disable('command')
-            }
-        }, 1000);
     });
 }
 
 if (disconnect) {
     disconnect.addEventListener('click', () => {
-        disable('connect')
+        socket.disconnect();
+        statusText.innerHTML = 'Status: Disconnected';
+        statusText.style.color = '#d93025';
         disable('disconnect')
-        disable('retry')
+        enable('connect')
         disable('send')
         disable('command')
-        socket.emit('_disconnect');
-        statusText.style.color = '#fc8c03';
-        statusText.innerHTML = 'Disconnecting...';
-        // Send request to restart server
-        location.href = '/restart';
-        restartWindow.close();
-    });
-}
-
-if (retry) {
-    retry.addEventListener('click', () => {
-        // Send request to restart server
-        location.href = '/restart';
-        restartWindow.close();
-        disable('connect')
-        disable('disconnect')
-        disable('retry')
-        statusText.style.color = '#fc8c03';
-        statusText.innerHTML = 'Connecting...';
-        // Check if connection is stuck
-        setTimeout(() => {
-            if (statusText.innerHTML == 'Connecting...') {
-                statusText.innerHTML = 'Error: Unable to locate a serial port';
-                statusText.style.color = '#d93025';
-                disable('connect')
-                disable('disconnect')
-                enable('retry')
-            }
-        }, 1000);
     });
 }
 
 if (command && send) {
-    console.log('Command and send found');
     // Check if enter key is pressed
     command.addEventListener('keypress', (event) => {
         if (command.value == '') return;
@@ -162,35 +79,25 @@ if (command && send) {
             command.classList.add('invalid');
             setTimeout(() => {
                 command.classList.remove('invalid');
-            }, 5000);
+            }, 1000);
             return;
         }
         // Create p element
         let p = document.createElement('p');
         p.innerHTML = `> ${command.value.toString()}<br>`;
         output.appendChild(p);
-        socket.emit('command', command.value);
+        socket.emit('data', command.value);
         command.value = '';
     });
 }
 
-socket.on('data', (data) => {
-    // Create p element
-    let p = document.createElement('p');
-    p.innerHTML = `${data}<br>`;
-    output.appendChild(p);
-});
-
 function disable (item) {
     switch (item) {
-        case 'connect':
-            connect.disabled = true;
-        break;
         case 'disconnect':
             disconnect.disabled = true;
         break;
-        case 'retry':
-            retry.disabled = true;
+        case 'connect':
+            connect.disabled = true;
         break;
         case 'command':
             command.disabled = true;
@@ -203,14 +110,11 @@ function disable (item) {
 
 function enable (item) {
     switch (item) {
-        case 'connect':
-            connect.disabled = false;
-        break;
         case 'disconnect':
             disconnect.disabled = false;
         break;
-        case 'retry':
-            retry.disabled = false;
+        case 'connect':
+            connect.disabled = false;
         break;
         case 'command':
             command.disabled = false;
